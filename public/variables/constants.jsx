7c91b021dec5488924cada28ff99f6bd37a371c9
Aye Myat Thin aung <ayemyatthin.aung@hiveinnovate.com>
2022-01-26 10:24:24 +0630

    add changes

--- public/variables/constants.jsx
+++ public/variables/constants.jsx
@@ -1,9 +1,9 @@
 export const ANIMATE_DELAY_TIME = 100;
 export const TITLE = 'Achromex';
-export const WEB_URL = 'https://www.achromex.com/';
-export const FACEBOOK_URL = 'https://www.facebook.com/achromex';
-export const FACEBOOK = 'achromexmm';
-export const TWITTER = '@achromexmm';
+export const WEB_URL = 'https://www.test.com/';
+export const FACEBOOK_URL = 'https://www.facebook.com/test';
+export const FACEBOOK = 'test';
+export const TWITTER = '@test';
 export const DESCRIPTION =
   'Ipsum aute cupidatat do consequat aliqua labore pariatur deserunt tempor consequat qui.';
 export const KEYWORDS =
@@ -13,7 +13,7 @@ export const LOCATION = {
   lat: 16.776956,
   lng: 96.160031,
 };
-export const COMPANY_MAP_URL = 'https://g.page/achromex?share';
+export const COMPANY_MAP_URL = 'https://g.page/test?share';
 export const EMAIL_LINK = 'hello@lva.com.mm';
 export const CONTACT = {
   primaryContact: '09 777 777 777',
@@ -21,6 +21,6 @@ export const CONTACT = {
 };
 export const YOUTUBE_URL =
   'https://www.youtube.com/channel/UCogvzWsh4W2GaUPZEI89BLw';
-export const IG_URL = 'https://www.instagram.com/fratello_myanmar/';
+export const IG_URL = 'https://www.instagram.com/test/';
 export const MAP_API_KEY = 'AIzaSyDmqa4GpER_guZzk2h4QwdbqoR6yzhD1_o';
 export const OG_IMAGE = '/image/logo.png';
