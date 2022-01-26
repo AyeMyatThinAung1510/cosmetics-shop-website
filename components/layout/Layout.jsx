a9d45985d3628823600fd67deca75be5312b0bca
Aye Myat Thin aung <ayemyatthin.aung@hiveinnovate.com>
2022-01-26 08:50:18 +0630

    change layout

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,26 +1,10 @@
-import React from "react";
-import NProgress from "nprogress";
-import Router from "next/router";
-import Navbar from "../navbar/Navbar";
-import Footer from "../footer/Footer";
-import ScrollUp from "../scrollUp/ScrollUp";
-import CopyRight from "../footer/CopyRight";
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer';
+import ScrollUp from '../scrollUp/ScrollUp';
+import CopyRight from '../footer/CopyRight';
 
 export default class Layout extends React.Component {
-  componentDidMount() {
-    Router.onRouteChangeStart = url => {
-      NProgress.start();
-    };
-
-    Router.onRouteChangeComplete = () => {
-      NProgress.done();
-    };
-
-    Router.onRouteChangeError = () => {
-      NProgress.done();
-    };
-  }
-
   render() {
     const { children, active } = this.props;
     return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
 import App from 'next/app';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+//external css
 import 'react-simple-flex-grid/lib/main.css';
 import 'react-slideshow-image/dist/styles.css';
-import '../public/scss/main.scss';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+//main css
+import '../public/scss/main.scss';
+import NProgress from 'nprogress';
+
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      NProgress.start();
+    });
+    router.events.on('routeChangeComplete', () => {
+      NProgress.done();
+    });
+    router.events.on('routeChangeError', () => NProgress.done());
+  });
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
